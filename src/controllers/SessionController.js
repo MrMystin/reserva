5d7f334ac716a8f163b9f74ba386d232496fdd0f
GABRIEL <cg3026434@labA204m33.IFSP>
2024-05-13 08:44:04 -0300

    Criação dos models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+  index (listAll): listagem de sessões
+  store (add): criar uma sessão
+  show (list): quando queremos listar uma unica sessão
+  update: quando queremos alterar alguma sessão
+  destroy (delete): quando queremos deletar uma sessão
+*/
+
+import * as Yup from 'yup'
+import User from '../models/User'
+
+class SessionController {
+  async store(req,res) {
+
+  }
+}
